83f330f9b1ef065a791a8433dd95b38b0dc43f31
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2022-04-05 01:02:38 -0400

    final update
    added comments

--- src/main/java/com/mycompany/statecapitals2/StateCapitals2.java
+++ src/main/java/com/mycompany/statecapitals2/StateCapitals2.java
@@ -8,21 +8,25 @@ package com.mycompany.statecapitals2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
 /**
- *
+ * This program reads from a text file, adds text to a hashmap.
+ * User is prompted to play a game of guessing the capitals of a state using random numbers.
+ * 
  * @author chica
  */
 public class StateCapitals2 {
 
     public static void main(String[] args) throws IOException {
         Map<String, String> capitals = new HashMap<>();//creating hashmap for the capitals
-        String userAns;//used to get answer for game question later
         
         //scanner is used to read from a file
         Scanner sc = new Scanner(new BufferedReader(new FileReader("Capitals.txt")));
@@ -34,7 +38,7 @@ public class StateCapitals2 {
             //index 0 should always hold the state, index 1 should hold the capital
             capitals.put(both[0], both[1]);
         }
-       
+        
         System.out.println("The size of my map: " + capitals.size());
         
         //create a set based off the Key's in the Capitals hashmap
@@ -47,26 +51,82 @@ public class StateCapitals2 {
             System.out.println(s);
         }   
         
+        //scanner used to read input from user
+        Scanner read = new Scanner(System.in);
+        int games = 0; //will be used to save the input from user
+        boolean valid = false; //for data validation
+        do { 
+            try {
+                System.out.print("How many states would you like to guess? ");
+                games = read.nextInt();
+                if(games >= 0 && games <= 50) { //as long as the number is not negative the loop will end
+                    valid = true;
+                }
+            } catch(InputMismatchException e) {
+                System.out.println("Please try again and type a whole integer");
+                read.next();
+            }
+        } while(!valid);
+        
+        //list of state index's that have been used in playing the game below
+        List<Integer> removedStates = new ArrayList<>();
+        for(int i = 0; i < games; i++) { 
+            //startGame returns the index of the state in the states Set
+            removedStates.add(startGame(states, capitals, removedStates));
+        }
+        sc.close();
+    }
+    
+    
+    /**
+     * Randomly generates a number, checks if the number has been used before
+     * Runs a loop until a new number is selected. 
+     * 
+     * @param states    a set of the 50 states
+     * @param capitals  a map of the 50 states along with their capitals
+     * @param removed   a list of indices of states that have been used
+     * @return          the index of the state randomly chosen
+     */
+    public static int startGame(Set<String> states, Map<String, String> capitals, List<Integer> removed) {
         //randomly selecting a state
         Random rand = new Random();
-        int stateIndex = rand.nextInt(states.size());
-        int count = 0;//count is used as an index counter
-        for(String s : states) {//advanced looping thru the states set
-            if(count == stateIndex) {//once the index is equal to the random number it prints this out: 
-                Scanner in = new Scanner(System.in);//used to get the answer from user
-                System.out.println("Do you know the capital of " + s + "?");
-                userAns = in.nextLine();
-                if(userAns.equalsIgnoreCase(capitals.get(s))) {//compares the answer given by user to hash map capital
-                    System.out.println("Correct!");
+        int stateIndex = rand.nextInt(states.size()); 
+        while(removed.contains(stateIndex)) {//if the removed list already has that index it will not pass this while loop
+            stateIndex = rand.nextInt(states.size());
+        }
+        gamePrompts(states, capitals, stateIndex);//the next method for the game
+        return stateIndex;   
+    }
+    
+    /**
+     * Uses a random number to prompt user to input capital of selected state
+     * Compares user input to the correct capital taken from capitals hashMap
+     * User then gets prompted if they were correct or not
+     * 
+     * @param states    A set of 50 states
+     * @param capitals  A map of 50 states along with their capitals
+     * @param stateIndex index of the randomly chosen state from the previous method
+     */
+    public static void gamePrompts(Set<String> states, Map<String, String> capitals, int stateIndex) {
+        int count = 0; //count is used as an index counter
+        String userAns = "";
+        Scanner in = new Scanner(System.in);
+            for(String s : states) {//advanced looping thru the states set
+                if(count == stateIndex) {//once the index is equal to the random number it prints this out: 
+                    //used to get the answer from user
+                    System.out.println("\n********************\n");
+                    System.out.println("Do you know the capital of " + s + "?");//s is the randomly selected state here
+                    userAns = in.nextLine();
+                    
+                    String ans = capitals.get(s); //this is the actual correct answer
+                    if(userAns.equalsIgnoreCase(ans)) {//compares the answer given by user to hashmap capital value
+                        System.out.println("Correct!");
+                    }
+                    else {
+                        System.out.println("Incorrect! The correct answer is: " + ans);
+                    }
                 }
-                else {
-                    System.out.println("Incorrect, the correct answer is " + capitals.get(s));
-                }
-                in.close();
+                count++;
             }
-            
-            count++;
-        }        
-        sc.close();
     }
 }
